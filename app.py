ff8b5e159e189cf7fbe54e0e212c07e07b5dc085
Italo Barros <italobbarros@gmail.com>
2023-03-04 13:33:54 -0300

    add: app.py include

---
+++ app.py
@@ -0,0 +1,12 @@
+from aes import crypto_aes
+
+def main():
+    try:
+        crypt = crypto_aes()
+        crypt.run_encrypt()
+        crypt.run_decrypt()
+    except Exception as e:
+        print(e)
+
+if (__name__ == "__main__"):
+    main()
